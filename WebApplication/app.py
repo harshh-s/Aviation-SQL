53ae6a42104a9d495006b8c94757075c2525dbe5
Harsh Seksaria <harsh20.hs@gmail.com>
2020-12-20 23:08:41 +0530

    Basic connection with azure done, taking variable values from one form, querying accordingly and then reading rows

--- WebApplication/app.py
+++ WebApplication/app.py
@@ -3,13 +3,38 @@ import pyodbc
 
 app = Flask(__name__)
 
-@app.route('/')
+server = 'dbt-lab.database.windows.net'
+database = 'Aviation'
+username = 'dbtlab'
+password = 'alpzulAZ5'   
+driver= '{ODBC Driver 17 for SQL Server}'
+conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    user = {'username' : 'Harsh'}
-    return render_template('index.html', user=user)
+    return render_template('index.html')
+
+@app.route('/flights', methods=['GET', 'POST'])
+def flights():
+    if request.method == "POST":
+        src = request.form['source']
+        dest = request.form['destination']
+        xyz = "SELECT * FROM flight WHERE flightsource = '" + src + "' AND flightdest = '" + dest +"'"
+
+        with conn.cursor() as cursor:
+            cursor.execute(xyz)
+            data = cursor.fetchall()
+            # while row:
+            #     print (str(row[0]) + " " + str(row[1]))
+            #     row = cursor.fetchone()
+            
+        return render_template('flights.html', data=data)
+    
+    else:
+        return render_template('index.html')
 
 @app.route('/destination')
-def dest():
+def destination():
     return render_template('destination.html')
 
 @app.route('/pricing')
