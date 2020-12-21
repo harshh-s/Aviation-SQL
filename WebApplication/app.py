3b2d401b2cee9cd5d11148cd5390ab7f931ea0dd
Harsh Seksaria <harsh20.hs@gmail.com>
2020-12-21 21:23:24 +0530

    Retrieve flights from a source to destination completed with backend and set in UI

--- WebApplication/app.py
+++ WebApplication/app.py
@@ -3,23 +3,22 @@ import pyodbc
 
 app = Flask(__name__)
 
-server = 'dbt-lab.database.windows.net'
-database = 'Aviation'
-username = 'dbtlab'
-password = 'alpzulAZ5'   
-driver= '{ODBC Driver 17 for SQL Server}'
-conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
+
+#index.html
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+#flights.html
 @app.route('/flights', methods=['GET', 'POST'])
 def flights():
     if request.method == "POST":
         src = request.form['source']
         dest = request.form['destination']
-        xyz = "SELECT * FROM flight WHERE flightsource = '" + src + "' AND flightdest = '" + dest +"'"
+        date = request.form['date']
+        print(src, dest, date)
+        xyz = "SELECT * FROM flight WHERE flightsource = '" + src + "' AND flightdest = '" + dest +"';"
 
         with conn.cursor() as cursor:
             cursor.execute(xyz)
@@ -29,23 +28,25 @@ def flights():
             #     row = cursor.fetchone()
             
         return render_template('flights.html', data=data)
-    
     else:
         return render_template('index.html')
 
+#destination.html
 @app.route('/destination')
 def destination():
     return render_template('destination.html')
+
 
 @app.route('/pricing')
 def pricing():
     return render_template('pricing.html')
 
+#contact.html
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-
+#
 if __name__ == '__main__':
     app.debug = True
     app.run()
